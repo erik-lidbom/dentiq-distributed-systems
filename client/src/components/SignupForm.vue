8915b6e202ebc3221ab0a68d331eafb5c809a5ee
eriklidb <eriklidb@student.chalmers.se>
2024-11-14 15:45:46 +0100

    #1 - Add Signup Screen GUI
    Create Signup Screen component in views directory that contains the GUI. The component contains additional children components that is added to the component directory.
    
    Additional note: Currently only contains GUI, no functionality implemented.

---
+++ client/src/components/SignupForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="flex flex-col gap-14">
+    <SignupHeader />
+    <form class="flex flex-col gap-2 w-full">
+      <div class="w-full relative lg:px-9">
+        <font-awesome-icon
+          icon="fa-solid fa-user"
+          class="absolute lg:left-14 left-4 top-1/2 transform -translate-y-1/2 text-dentiqGray"
+        />
+        <input
+          class="w-full rounded-2xl bg-dentiqLightGray lg:pl-12 pl-9 py-3 text-dentiqGray placeholder:text-dentiqGray bg-opacity-30"
+          type="text"
+          placeholder="Full Name"
+        />
+      </div>
+      <div class="w-full relative lg:px-9">
+        <font-awesome-icon
+          icon="fa-solid fa-shield-halved"
+          class="absolute lg:left-14 left-4 top-1/2 transform -translate-y-1/2 text-dentiqGray"
+        />
+        <input
+          class="w-full rounded-2xl bg-dentiqLightGray lg:pl-12 pl-9 py-3 text-dentiqGray placeholder:text-dentiqGray bg-opacity-30"
+          type="text"
+          placeholder="Personnummer"
+        />
+      </div>
+      <div class="w-full relative lg:px-9">
+        <font-awesome-icon
+          icon="fa-solid fa-envelope"
+          class="absolute lg:left-14 left-4 top-1/2 transform -translate-y-1/2 text-dentiqGray"
+        />
+        <input
+          class="w-full rounded-2xl bg-dentiqLightGray lg:pl-12 pl-9 py-3 text-dentiqGray placeholder:text-dentiqGray bg-opacity-30"
+          type="text"
+          placeholder="Email"
+        />
+      </div>
+      <div class="w-full flex lg:flex-row flex-col items-center justify-center gap-2 lg:px-9">
+        <div class="w-full relative">
+          <font-awesome-icon
+            icon="fa-solid fa-lock"
+            class="absolute left-5 top-1/2 transform -translate-y-1/2 text-dentiqGray"
+          />
+          <input
+            class="w-full rounded-2xl bg-dentiqLightGray text-dentiqGray py-3 pl-11 placeholder:text-dentiqGray bg-opacity-30"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="w-full relative">
+          <font-awesome-icon
+            icon="fa-solid fa-lock"
+            class="absolute left-5 top-1/2 transform -translate-y-1/2 text-dentiqGray"
+          />
+          <input
+            class="w-full rounded-2xl bg-dentiqLightGray text-dentiqGray py-3 pl-11 placeholder:text-dentiqGray bg-opacity-30"
+            type="password"
+            placeholder="Confirm password"
+          />
+        </div>
+      </div>
+    </form>
+
+    <div class="w-full flex flex-col gap-5 lg:px-9">
+      <div class="flex items-center justify-start font-thin gap-2">
+        <p class="text-dentiqGray">Already have an account?</p>
+        <router-link
+          to="/login"
+          class="text-dentiqLightBlue hover:text-opacity-30 transition-all duration-300"
+        >
+          Log in
+        </router-link>
+      </div>
+      <button
+        class="w-full rounded-xl bg-dentiqBtnBlue text-white font-thin py-3 hover:bg-opacity-30 transition-all duration-300"
+        type="password"
+        placeholder="Confirm password"
+      >
+        Create Account
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SignupHeader from './SignupHeader.vue'
+</script>
---
+++ client/src/components/SignupHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-2">
+    <p class="font-bold text-white text-3xl text-center">Get Started with DentiQ</p>
+    <p class="text-dentiqGray text-xl text-center">
+      Join us and bring innovation to your dental practice!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
