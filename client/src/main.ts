b55e459f2c9d85cf5cc4b6268e8e767a736aca49
eriklidb <eriklidb@student.chalmers.se>
2024-11-14 15:27:28 +0100

    #1 - Add icons used in signup screen

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,8 +3,19 @@ import './assets/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import the fontawesome core
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-const app = createApp(App)
+// import font awesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// import specific icons
+import { faUser, faShieldHalved, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUser, faShieldHalved, faEnvelope, faLock)
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
