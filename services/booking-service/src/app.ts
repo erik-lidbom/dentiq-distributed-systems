14af96a0000ea53a2b8a89d624607eebec788be7
Andreas <haatajaa@net.chalmers.se>
2024-11-20 18:14:09 +0100

    #19 Delete old folders

--- services/booking-service/src/app.ts
+++ services/booking-service/src/app.ts
@@ -1,47 +1,21 @@
 import express, { Express } from "express";
-import mqtt, { MqttClient, IClientOptions } from "mqtt";
 import dotenv from "dotenv";
+import mqttClient from "./mqtt/mqtt";
+
+mqttClient
 
 dotenv.config();
 
-// Initialize the Express app
 const app: Express = express();
+const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json()); // Parse incoming JSON requests
+app.use(express.json());
 
-// MQTT
-const mqttConnOptions: IClientOptions = {
-  host: process.env.MQTT_HOST,
-  port: parseInt(process.env.MQTT_PORT || "8883", 10),
-  protocol: 'mqtts',
-  username: process.env.MQTT_USERNAME,
-  password: process.env.MQTT_PASSWORD
-};
-
-const mqttClient = mqtt.connect(mqttConnOptions);
-
-mqttClient.on("connect", () => {
-  console.log("[MQTT]: Successfully connected to the broker!\n");
-
-  // Example: Subscribe to a topic
-  mqttClient.subscribe(process.env.APPOINTMENT_TOPIC!, (err) => {
-    if (err) {
-      console.error("[mqtt]: Subscription error", err);
-    } else {
-      console.log("[mqtt]: Subscribed to topic", process.env.APPOINTMENT_TOPIC);
-    }
-  });
-});
-
-mqttClient.on("message", (topic, message) => {
-  console.log(`[mqtt]: Received message on ${topic}:`, message.toString());
-});
-
-// Routes
 app.get("/", (req, res) => {
-  res.send("Express + TypeScript App");
+  res.send("BASIC MQTT + EXPRESS + NODE + TS SETUP");
 });
 
-// Export the configured app
-export default app;
+app.listen(port, () => {
+  console.log(`[SERVER]: Server is running at http://localhost:${port}`);
+  console.log("-------------------------------------------------------")
+});
