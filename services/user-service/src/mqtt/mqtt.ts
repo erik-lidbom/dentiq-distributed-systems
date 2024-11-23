b83acda1d5334aa37b7ceffcbe84e06fcd64dd8a
Utkarsh <utkarshs@student.chalmers.se>
2024-11-23 19:09:52 +0100

    #15 Add connection for user-service

---
+++ services/user-service/src/mqtt/mqtt.ts
@@ -0,0 +1,32 @@
+import mqtt, { MqttClient, IClientOptions } from "mqtt";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mqttConnOptions: IClientOptions = {
+    host: process.env.MQTT_HOST,
+    port: parseInt(process.env.MQTT_PORT || "8883", 10),
+    protocol: 'mqtts',
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+  };
+  
+  const mqttClient: MqttClient = mqtt.connect(mqttConnOptions);
+  
+  mqttClient.on("connect", () => {
+    console.log("[MQTT]: Successfully connected to the broker!");
+  
+    mqttClient.subscribe(process.env.APPOINTMENT_TOPIC!, (err) => {
+      if (err) {
+        console.error("[MQTT]: Could not establish subscription connection: ", err);
+      } else {
+        console.log("[MQTT]: Subscribed to topic", process.env.PATIENT_TOPIC);
+        console.log("-------------------------------------------------------");
+      }
+    });
+  });
+  
+  mqttClient.on("message", (topic, message) => {
+    console.log(`[MQTT]: Message recieved from ${topic}:`, message.toString());
+  });
+
+export default mqttClient;
