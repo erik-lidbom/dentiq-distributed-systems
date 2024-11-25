f50e626339293713ebe7de9312c265df86e6bcd0
nabil-alsayed <nabilsa@chalmers.se>
2024-11-26 00:18:14 +0100

    #4 Refactor Filter component layout and remove accessibility section for improved design

--- patient/src/components/Filter.vue
+++ patient/src/components/Filter.vue
@@ -1,20 +1,11 @@
 <template>
-    <div class="h-full rounded-2xl p-2 w-[228px] space-y-8 bg-dentiq-muted-lightest">
+    <div class="h-full rounded-2xl p-6 w-[250px] space-y-8 bg-dentiq-muted-lightest">
       <!-- Header -->
       <h2 class="text-dentiq-h4 font-semibold text-dentiq-muted-darkest">Filter by</h2>
       
       <!-- Services List -->
        <div class="flex flex-col space-y-4">
         <h3 class="text-dentiq-body font-semibold text-dentiq-muted-darkest">Services</h3>
-        <hr>
-        <div class="text-dentiq-body-small text-dentiq-muted-darker flex-wrap gap-1 flex">
-            <h3 v-for="(service, index) in services" :key="index" class=" px-2 py-1 rounded-md bg-dentiq-muted-light w-fit">{{ service.name }}</h3>
-        </div>
-      </div>
-
-      <!-- Accessability -->
-      <div class="flex flex-col space-y-4">
-        <h3 class="text-dentiq-body font-semibold text-dentiq-muted-darkest">Accessability</h3>
         <hr>
         <div class="text-dentiq-body-small text-dentiq-muted-darker flex-wrap gap-1 flex">
             <h3 v-for="(service, index) in services" :key="index" class=" px-2 py-1 rounded-md bg-dentiq-muted-light w-fit">{{ service.name }}</h3>
