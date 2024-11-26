6cb1d28b6573598371678b529add3abc190fced4
nabil-alsayed <nabilsa@chalmers.se>
2024-11-26 21:40:00 +0100

    #20 Refactor ClinicMapCard.vue for responsive design and improved styling

--- patient/src/components/ClinicMapCard.vue
+++ patient/src/components/ClinicMapCard.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Placeholder Image -->
-    <div class="absolute flex flex-col left-14 top-0 items-center bg-white rounded-3xl max-h-[480px] w-[310px] overflow-hidden shadow-lg shadow-dentiq-muted-light border-dentiq-border-shadowStrenghter border-[2px] font-archivo">
+    <div class="absolute flex flex-col left-14 top-0 items-center bg-white rounded-3xl max-h-[480px] w-[200px] min-[425px]:w-[310px] overflow-hidden shadow-lg shadow-dentiq-muted-light border-dentiq-border-shadowStrenghter border-[2px] font-archivo">
       <div class="w-full max-h-[220px] bg-dentiq-muted-lighter justify-center items-center overflow-hidden">
         <img
         :src="clinic.image || defaultPlaceholder"
@@ -8,26 +8,26 @@
         class="w-[100%] h-[100%]"
         />
       </div>
-      <div class="w-full p-4 space-y-[20px]">
+      <div class="w-full p-2 min-[425px]:p-4 space-y-[10px] min-[425px]:space-y-[20px]">
         
         <div class="flex flex-col w-full justify-center items-start space-y-[5px]">
-          <h1 class="font-semibold w-fit text-dentiq-h3">{{ clinic.name }}</h1>
-          <h2 class="font-normal w-full text-dentiq-body-large text-dentiq-muted-default">{{ clinic.address }}</h2>
+          <h1 class="font-semibold w-fit text-dentiq-h4 min-[425px]:text-dentiq-h3">{{ clinic.name }}</h1>
+          <h2 class="font-normal w-full text-dentiq-body-small min-[425px]:text-dentiq-body text-dentiq-muted-default">{{ clinic.address }}</h2>
         </div>
 
-        <div class="flex flex-col items-center w-full space-y-[20px]">
+        <div class="flex flex-col items-center w-full space-y-[10px] min-[425]:space-y-[20px]">
           
           <div class="flex flex-row w-full">
-            <h2 class="font-normal w-full text-dentiq-body text-dentiq-muted-default">First Availabillity</h2>
+            <h2 class="font-normal w-full text-dentiq-caption min-[425px]:text-dentiq-body text-dentiq-muted-default">First Availabillity</h2>
             <div class="flex flex-row w-full space-x-[5px] justify-center items-center">
               <FontAwesomeIcon icon="circle" class="text-green-500" />
-              <h2 class="font-normal w-fit text-dentiq-body">{{ clinic.firstAvailableTime }}</h2>
+              <h2 class="font-normal w-fit text-dentiq-caption min-[425px]:text-dentiq-body">{{ clinic.firstAvailableTime }}</h2>
             </div>
           </div>
 
-          <div class="flex flex-wrap w-full gap-[10px]">
-            <p v-for="service in visibleServices" class="text-dentiq-body-small px-2 py-[7px] rounded-lg text-dentiq-muted-darker bg-dentiq-muted-lighter">{{ service }}</p>
-            <p v-if="hiddenServices.length > 0" class="text-dentiq-body-small px-2 py-[7px] rounded-lg text-dentiq-muted-darker bg-dentiq-muted-lighter">+{{ hiddenServices.length }}</p>
+          <div class="flex flex-wrap w-full gap-[5px] min-[425px]:gap-[10px]">
+            <p v-for="service in visibleServices" class="text-dentiq-caption-small min-[425px]:text-dentiq-body-small px-2 py-[7px] rounded-lg text-dentiq-muted-darker bg-dentiq-muted-lighter">{{ service }}</p>
+            <p v-if="hiddenServices.length > 0" class="text-dentiq-caption-small min-[425px]:text-dentiq-body-small px-2 py-[7px] rounded-lg text-dentiq-muted-darker bg-dentiq-muted-lighter">+{{ hiddenServices.length }}</p>
           </div>
 
         </div>
