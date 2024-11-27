5ac334287e0ab60c2bca464266400b077c5c19fd
nabil-alsayed <nabilsa@chalmers.se>
2024-11-27 08:52:43 +0100

    #20 Add Back the firstAvailableTime and dentists information to clinic data

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const clinics = [
     {
       id: 1,
@@ -18,6 +17,8 @@ const clinics = [
       lng: 11.9746,
       address: "123 Main St",
       services: ["Dentist", "Orthodontist", "Endodontist"],
+      firstAvailableTime: "2024 Dec 22",
+      dentists: ["950202-1231", "900202-1232", "930202-1233", "940202-1234", "960202-1235", "970202-1236", "980202-1237"]
     },
     {
       id: 2,
@@ -26,6 +27,8 @@ const clinics = [
       lng: 11.9668,
       address: "456 Elm St",
       services: ["Dentist", "Orthodontist", "Endodontist", "Oral Surgeon", "Pediatric Dentist", "Periodontist", "Prosthodontist"],
+      firstAvailableTime: "2024 Dec 22",
+      dentists: ["820402-1421", "830402-1422", "840402-1423", "850402-1424", "860402-1425", "870402-1426", "880402-1427"]
     },
   ];
 
