10f3ebbfe0631e5d0836b598d08156ac732d0c8a
nabil-alsayed <nabilsa@chalmers.se>
2024-11-29 00:12:07 +0100

    #21 Add TODO comment for future removal of fake service in service registry

--- api-gateway/services/serviceRegistery.ts
+++ api-gateway/services/serviceRegistery.ts
@@ -8,7 +8,7 @@ interface Service {
 const services: Record<string, Service> = {
   user: { name: "user", host: "http://localhost", port: 3001, url: "http://localhost:3001/api" },
   booking: { name: "booking", host: "http://localhost", port: 3002, url: "http://localhost:3002/api/appointments" },
-  fake: { name: "fake", host: "http://localhost", port: 3002, url: "http://localhost:3002/api" },
+  fake: { name: "fake", host: "http://localhost", port: 3002, url: "http://localhost:3002/api" }, // TODO: Remove this later
 };
 
 export const getService = (serviceName: string): Service | undefined => {
