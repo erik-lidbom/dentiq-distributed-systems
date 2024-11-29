c07464dfb03c465f0f337f26eb462a7beeaa0b69
nabil-alsayed <nabilsa@chalmers.se>
2024-11-29 13:49:51 +0100

    #12 Add initial setup for dentist application with routing, styling, and basic components

---
+++ dentist/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-row min-h-full rounded-2xl p-0 bg-dentiq-muted-lightest space-x-4 overflow-hidden relative">
+    <h2>Dentist</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Map from "@/components/Map.vue";
+
+</script>
