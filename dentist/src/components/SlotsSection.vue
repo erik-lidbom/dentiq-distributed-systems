8515dfd85c892c81e622c91967abe2c50126e512
Lilly <lilly@chalmers.se>
2024-11-29 15:46:14 +0100

    #12 Add titles for slot component

--- dentist/src/components/SlotsSection.vue
+++ dentist/src/components/SlotsSection.vue
@@ -2,5 +2,11 @@
 </script>
 
 <template> 
-    <div class="bg-black min-h-full w-2/3 relative"></div>
+    <div class="min-h-full w-2/3 relative border-r border-gray-400">
+        <!-- Title and Description -->
+         <div class="ml-5 w-4/5 border-b border-gray-400">
+            <h2 class="pt-4 text-dentiq-h2">My Slots</h2>
+            <p class="pb-3 text-dentiq-body-small  italic text-gray-500">Manage available slots, if there are specific time slots you dont want clients to see, disable them.</p>
+        </div>
+    </div>
 </template>
