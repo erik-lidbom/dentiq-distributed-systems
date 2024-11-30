7b6c3e55b45de3acaf51d847db82519e7305b73d
eriklidb <eriklidb@student.chalmers.se>
2024-12-01 00:28:42 +0100

    #17 - Add subscribe.ts file that is responsible to listen to subscribed topics

---
+++ services/notification-service/src/mqtt/subscribe.ts
@@ -0,0 +1,28 @@
+import { client } from "./mqtt";
+import { TOPICS } from "./topics";
+
+export const subscribeTopics = async () => {
+  client?.subscribe(TOPICS.SUBSCRIBE.APPOINTMENT_CREATED, (err) => {
+    if (!err) {
+      console.log(`Subscribed to ${TOPICS.SUBSCRIBE.APPOINTMENT_CREATED}`);
+      return;
+    }
+    console.error("MQTT Subscription error:", err);
+  });
+  client?.subscribe(TOPICS.SUBSCRIBE.APPOINTMENT_BOOKED, (err) => {
+    if (!err) {
+      console.log(`Subscribed to ${TOPICS.SUBSCRIBE.APPOINTMENT_BOOKED}`);
+      return;
+    }
+    console.error("MQTT Subscription error:", err);
+  });
+  client?.subscribe(TOPICS.SUBSCRIBE.APPOINTMENT_CANCEL_CONFIRMATION, (err) => {
+    if (!err) {
+      console.log(
+        `Subscribed to ${TOPICS.SUBSCRIBE.APPOINTMENT_CANCEL_CONFIRMATION}`
+      );
+      return;
+    }
+    console.error("MQTT Subscription error:", err);
+  });
+};
