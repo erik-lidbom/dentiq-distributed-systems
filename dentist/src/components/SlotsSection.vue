855521ec72f4d5060c9968fa438b2da13427dacf
Lilly <lilly@chalmers.se>
2024-12-01 17:22:10 +0100

    #12 Fix calendar icon and calendar view bug

--- dentist/src/components/SlotsSection.vue
+++ dentist/src/components/SlotsSection.vue
@@ -4,16 +4,18 @@
         <!-- Title and Description -->
          <div class="border-b border-gray-400">
             <h2 class="pt-4 text-dentiq-h2">My Slots</h2>
-            <p class="pb-3 text-dentiq-body-small  italic text-gray-500">Manage available slots, if there are specific time slots you dont want clients to see, disable them.</p>
+            <p class="pb-3 text-dentiq-body-small italic text-gray-500">Manage available slots, if there are specific time slots you dont want clients to see, disable them.</p>
          </div>
         <!-- Date Display -->
         <div class="flex items-center justify-between mb-4 relative pt-3">
             <h4 class="text-dentiq-h4">{{ formattedDate }}</h4>
-            <button @click="toggleCalendar" class="py-1 px-3 bg-blue-600 text-white text-sm rounded hover:bg-blue-700 transition"> Cal </button>
+            <button @click="toggleCalendar">
+              <i class="pi pi-calendar text-gray-500 text-xl"></i> 
+            </button>
         
           <!-- Calendar Display -->
           <div v-if="showCalendar" class="absolute bg-white shadow-lg rounded-md p-4 z-10 border border-gray-400 top-full right-0 mt-2">
-              <Datepicker v-model="selectedDate" @change="onDateChange"></Datepicker>
+              <Datepicker v-model="selectedDate" @change="onDateChange" @closed="showCalendar = false"/>
           </div>
         </div>
         <!-- Slots Display -->
