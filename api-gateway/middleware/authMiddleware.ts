469bfcfc41af0ee9e2795a0ad975f4b81b46c20d
nabil-alsayed <nabilsa@chalmers.se>
2024-12-01 08:11:16 +0100

    #21 Add authentication middleware to API Gateway routes

---
+++ api-gateway/middleware/authMiddleware.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import { User } from '../types';
+
+// Authentication middleware
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Authorization header missing' });
+  }
+
+   // For "Bearer <token>"
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const response = await axios.post('http://localhost:4001/validate-token', { token });
+    if (response.data.valid) {
+      // Attach user info to the request
+      req.user = response.data.user as User;
+      next();
+    } else {
+      res.status(401).json({ error: 'Invalid token' });
+    }
+  } catch (error) {
+    console.error('Error validating token:', (error as Error).message);
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+
+export default authMiddleware;
--- api-gateway/routes/index.ts
+++ api-gateway/routes/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import routingController from '../controllers/routingController';
+import authMiddleware from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 // Dynamic route to handle optional path
-router.all('/:serviceName/:path*?', routingController);
+router.all('/:serviceName/:path*?', authMiddleware, routingController);
 
 export default router;
