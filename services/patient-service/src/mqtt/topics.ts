6acfe4732df7768cdff53d8aed3ae4553c81dde9
Utkarsh <utkarshs@student.chalmers.se>
2024-12-05 07:31:29 +0100

    #25 Add topics file patient service

---
+++ services/patient-service/src/mqtt/topics.ts
@@ -0,0 +1,9 @@
+export const TOPICS = {
+    SUBSCRIBE: {
+      PATIENT_BOOKING: "dentiq/appointmentService/patientService/bookSlot",
+    },
+    PUBLISH: {
+      PATIENT_AWAIT_CONFIRMATION: "dentiq/patientService/appointmentService/bookSlot/status"
+    },
+  };
+  
