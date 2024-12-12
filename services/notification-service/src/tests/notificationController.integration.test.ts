1baf4b13b2de8d7f977e34736006e84ac0ee26a1
eriklidb <eriklidb@student.chalmers.se>
2024-12-12 20:43:34 +0100

    #26 - Add unit and integration test for notification service

---
+++ services/notification-service/src/tests/notificationController.integration.test.ts
@@ -0,0 +1,51 @@
+import { afterAll, beforeAll, describe, expect, it } from "@jest/globals";
+import { createNotification } from "../controllers/controller"; // Adjust the path as necessary
+import mongoose from "mongoose";
+
+/*
+ * Tests for creating a notification and sotiring in database
+ */
+
+// Mock test data
+const TEST_DATA = Buffer.from(
+  JSON.stringify({
+    email: "testuser@example.com",
+    message: "This is a test notification.",
+    senderService: "testService",
+    patientId: "123",
+    dentistId: "456",
+  })
+);
+
+describe("Integration Test: createNotification", () => {
+  beforeAll(async () => {
+    // Connects to an in-memory MongoDB instance
+    await mongoose.connect("mongodb://localhost/test-db");
+  });
+
+  afterAll(async () => {
+    // Disconnects from the database after running all tests
+    await mongoose.connection.close();
+  });
+
+  it("should create and save a notification successfully", async () => {
+    // Create a notification document by calling creatingNotification
+    const savedNotification = await createNotification(TEST_DATA);
+
+    // Check if the notification was successfully stored and returned
+    expect(savedNotification).toHaveProperty("_id");
+    expect(savedNotification.email).toBe("testuser@example.com");
+    expect(savedNotification.message).toBe("This is a test notification.");
+    expect(savedNotification.senderService).toBe("testService");
+    expect(savedNotification.patientId).toBe("123");
+    expect(savedNotification.dentistId).toBe("456");
+  });
+
+  it("shall throw an error if data is invalid", async () => {
+    // Calls createNotification with invalid data (e.g., invalid JSON in Buffer)
+    const invalidData = Buffer.from("invalid data");
+
+    //Checks if the error is thrown
+    await expect(createNotification(invalidData)).rejects.toThrow();
+  });
+});
