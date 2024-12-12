69511b0bf2a168fcf52642779aadc79b3bf4c1e8
Lilly <lilly@chalmers.se>
2024-12-13 00:32:37 +0100

    #28 Create initial pieces for popup

--- dentist/src/components/CancelPopup.vue
+++ dentist/src/components/CancelPopup.vue
@@ -1,6 +1,17 @@
 <template>
     <div v-if="visible" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div class="bg-white w-2/5 h-2/3 rounded-lg"></div>
+        <div class="bg-white w-2/5 h-4/5 rounded-lg p-5 flex flex-col items-center justify-center">
+            <!-- Titles Section -->
+            <h1 class="text-dentiq-h2"> Are you sure? </h1>
+            <p class="text-dentiq-body text-center"> This action will cancel the booking and notify the patient. The cancelation cannot be undone. </p>
+            <!-- Type Name Section -->
+            <div class="mt-7 flex flex-col items-center justify-center"> 
+                <label for="instruction_text" class="mb-2 text-dentiq-body">Please type in the name of the patient to confirm.</label>
+                <label for="patient_name" class="mb-2 text-dentiq-body"> Name: Erik Lidbom</label>
+                <input type="text" id="confirm_name" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5" placeholder="Type here ..." required />
+                <button class="bg-red-500 px-5 py-2 rounded-lg text-white w-1/2 "> Confirm Cancellation </button>
+            </div>
+        </div>
     </div>
 </template>
   
