aea96c14e4adb399e240091fa7b14af67504a554
eriklidb <eriklidb@student.chalmers.se>
2024-12-14 11:52:27 +0100

    #31 - Add new topics

--- patient/src/mqtt/topics.ts
+++ patient/src/mqtt/topics.ts
@@ -2,6 +2,11 @@ export const TOPICS = {
   SUBSCRIBE: {
     NOTIFICATION_CREATED:
       'dentiq/notificationService/patientInterface/addNotification',
+    NOTIFICATION_ADDED_SLOT:
+      'dentiq/notificationService/patientService/addSlots',
+    NOTIFICATION_BOOKED_SLOT: 'dentiq/notificationService/patientClient/booked',
+    NOTIFICATION_CANCELLED_SLOT:
+      'dentiq/notificationService/patientClient/cancelSlot',
   },
   PUBLISH: {
     NOTIFICATION_REMOVED:
