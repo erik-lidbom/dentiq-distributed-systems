40370107e3ddfa1686f6b1cf4cdf545af63fe93a
Lilly <lilly@chalmers.se>
2024-12-14 14:00:50 +0100

    #2 Create title section and initial divs

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,3 +1,15 @@
 <template>
-    <div class="w-full lg:w-3/5 h-full border"></div>
+    <div class="w-full lg:w-3/5 h-full border flex flex-col justify-center items-center">
+        <div class="flex flex-col border border-green-400 w-auto h-auto">
+            <div class="flex items-center justify-center gap-2 m-10">
+                <img src="../assets/dentiq_logo.svg" alt="Dentiq logo" class="w-[30px] h-auto" />
+                <p class="font-bold text-white text-2xl">DentiQ</p>
+            </div>
+            <div class="flex flex-col items-center justify-center gap-2">
+                <p class="font-bold text-white text-3xl text-center">Welcome Back</p>
+                <p class="text-dentiqGray text-xl text-center"> Please log in to your account</p>
+            </div>
+        </div>
+        
+    </div>
 </template>
