df83f5a98fdbb37e20447df100d75c76271e6665
nabil-alsayed <nabilsa@chalmers.se>
2024-12-14 04:38:48 +0100

    #31 Add MQTT topics for notification creation and removal

---
+++ patient/src/mqtt/topics.ts
@@ -0,0 +1,10 @@
+export const TOPICS = {
+  SUBSCRIBE: {
+    NOTIFICATION_CREATED:
+      'dentiq/notificationService/patientInterface/addNotification',
+  },
+  PUBLISH: {
+    NOTIFICATION_REMOVED:
+      'dentiq/patientInterface/notificationService/removeNotification',
+  },
+};
