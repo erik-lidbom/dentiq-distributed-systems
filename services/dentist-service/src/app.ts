48d650dc5b689689d367bf12463da9bc6b0c7b78
nabil-alsayed <nabilsa@chalmers.se>
2024-12-29 08:07:35 +0100

    #46 Update main entry point to TypeScript and remove dentist routes

--- services/dentist-service/src/app.ts
+++ services/dentist-service/src/app.ts
@@ -1,8 +1,7 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
-import mqttClient from './mqtt/mqtt'; // MQTT client initialization
-import dentistRouter from './routes/dentistRoutes'; // Dentist routes
-import connectToDB from './db/db'; // Database connection function
+import mqttClient from './mqtt/mqtt';
+import connectToDB from './db/db';
 
 // Initialize environment variables
 dotenv.config();
@@ -18,27 +17,6 @@ mqttClient;
 
 // Middleware
 app.use(express.json());
-
-// Root Route for Testing
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'Dentist Microservice Running',
-    endpoints: {
-      dentists: '/api/dentists',
-    },
-  });
-});
-
-// Dentist Routes
-app.use('/api/dentists', dentistRouter);
-
-// Fallback Route for Undefined Routes
-app.use((req: Request, res: Response) => {
-  res.status(404).json({
-    error: 'Route not found',
-    message: 'Please check the available routes at the root URL `/`',
-  });
-});
 
 // Start the Server
 app.listen(port, () => {
