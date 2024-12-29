183a6cb70f11086c38c026250968dc9a3df5e822
nabil-alsayed <nabilsa@chalmers.se>
2024-12-29 08:07:19 +0100

    #46 Implement MQTT message handling for dentist CRUD operations and update subscription topics

--- services/dentist-service/src/mqtt/topics.ts
+++ services/dentist-service/src/mqtt/topics.ts
@@ -1,9 +1,25 @@
 export const TOPICS = {
   SUBSCRIBE: {
     DENTIST_CREATE_APP: 'dentiq/appointmentService/dentistService/addSlot',
+    CREATE: 'gateway/dentist/create/response',
+    GET: 'gateway/dentist/get/response',
+    UPDATE: 'gateway/dentist/update/response',
+    DELETE: 'gateway/dentist/delete/response',
+    QUERY: 'gateway/dentist/query/response',
+    CLINICS: {
+      QUERY_RESPONSE: 'gateway/clinic/query/response',
+    },
   },
   PUBLISH: {
     DENTIST_AWAIT_CONF:
       'dentiq/dentistService/appointmentService/addSlot/status',
+    CREATE_RESPONSE: 'dentist/create/response',
+    GET_RESPONSE: 'dentist/get/response',
+    UPDATE_RESPONSE: 'dentist/update/response',
+    DELETE_RESPONSE: 'dentist/delete/response',
+    QUERY_RESPONSE: 'dentist/query/response',
+    CLINICS: {
+      QUERY_RESPONSE: 'clinic/query/response',
+    },
   },
 };
