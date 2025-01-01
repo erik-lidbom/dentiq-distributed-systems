3f3f2f936abd2a7fcdedb43fb3e447c4cb458a34
Utkarsh <utkarshs@student.chalmers.se>
2025-01-01 21:58:40 +0100

    #7 Add topics to subscribe.ts file

--- services/dentist-service/src/mqtt/subscribe.ts
+++ services/dentist-service/src/mqtt/subscribe.ts
@@ -2,19 +2,18 @@ import { mqttClient } from "./mqtt";
 import { TOPICS } from "./topics";
 
 // Subscribe to topics
-const subscriptionTopics = [TOPICS.SUBSCRIBE.DENTIST_CREATE_APP];
+const subscriptionTopics = [
+  TOPICS.SUBSCRIBE.DENTIST_CREATE_APP,
+  TOPICS.SUBSCRIBE.CREDENTIAL_VALIDATION_REQUEST,
+];
 
 export const subscribeTopics = async () => {
   subscriptionTopics.map((topic: string) => {
     mqttClient?.subscribe(topic, (err) => {
       if (!err) {
         console.log(`Subscribed to ${topic}`);
-        return;
       } else {
-        console.log(
-          "[MQTT]: Subscribed to topics:",
-          subscriptionTopics.join(", ")
-        );
+        console.error(`[MQTT]: Error subscribing to ${topic}:`, err.message);
       }
     });
   });
