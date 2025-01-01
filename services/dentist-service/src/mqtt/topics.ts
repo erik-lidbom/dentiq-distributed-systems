721e7727f2f030c1b512acf905aa546a6e878909
Utkarsh <utkarshs@student.chalmers.se>
2025-01-01 21:59:13 +0100

    #7 add topics in topics.ts

--- services/dentist-service/src/mqtt/topics.ts
+++ services/dentist-service/src/mqtt/topics.ts
@@ -1,9 +1,12 @@
 export const TOPICS = {
   SUBSCRIBE: {
     DENTIST_CREATE_APP: 'dentiq/appointmentService/dentistService/addSlot',
+    CREDENTIAL_VALIDATION_REQUEST: 'dentiq/dentist/validate-credentials',
   },
   PUBLISH: {
     DENTIST_AWAIT_CONF:
       'dentiq/dentistService/appointmentService/addSlot/status',
+    CREDENTIAL_VALIDATION_RESPONSE: (correlationId: string) =>
+      `dentiq/authService/response/validate-credentials/${correlationId}`,
   },
 };
