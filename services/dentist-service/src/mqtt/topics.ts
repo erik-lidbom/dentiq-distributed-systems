8359dfee112f9df09eb98dcfc3ade1c33a36c456
Utkarsh <utkarshs@student.chalmers.se>
2025-01-03 23:03:43 +0100

    #49 fix bug and add jest

--- services/dentist-service/src/mqtt/topics.ts
+++ services/dentist-service/src/mqtt/topics.ts
@@ -4,9 +4,16 @@ export const TOPICS = {
     CREDENTIAL_VALIDATION_REQUEST: 'dentiq/dentist/validate-credentials',
   },
   PUBLISH: {
-    DENTIST_AWAIT_CONF:
-      'dentiq/dentistService/appointmentService/addSlot/status',
+    DENTIST_AWAIT_CONF: 'dentiq/dentistService/appointmentService/addSlot/status',
     CREDENTIAL_VALIDATION_RESPONSE: (correlationId: string) =>
       `dentiq/authService/response/validate-credentials/${correlationId}`,
+    CREATE_RESPONSE: 'dentiq/dentistService/create/response',
+    GET_RESPONSE: 'dentiq/dentistService/get/response',
+    UPDATE_RESPONSE: 'dentiq/dentistService/update/response',
+    DELETE_RESPONSE: 'dentiq/dentistService/delete/response',
+    QUERY_RESPONSE: 'dentiq/dentistService/query/response',
+    CLINICS: {
+      QUERY_RESPONSE: 'dentiq/clinicService/query/response',
+    },
   },
 };
