5a0624ecf14560e3647b1e49607f6599dfdbcb2d
nabil-alsayed <nabilsa@chalmers.se>
2025-01-03 23:18:53 +0100

    #47 Remove Unnecessary Services and Move it to /api For Consistency

--- patient/src/api/bookingService.ts
+++ patient/src/api/bookingService.ts
@@ -1,11 +1,12 @@
 const BASE_URL = 'http://localhost:3000/api/booking/query';
 
-export async function fetchAppointments(): Promise<any> {
+export async function fetchAppointments(body = {}): Promise<any> {
   const response = await fetch(BASE_URL, {
-    method: 'GET',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify(body),
   });
 
   if (!response.ok) {
@@ -13,4 +14,28 @@ export async function fetchAppointments(): Promise<any> {
   }
 
   return response.json();
+}
+
+export async function bookAppointment(body: any): Promise<any> {
+  try {
+    // Post an appointment
+    const response = await fetch(`${BASE_URL}/book`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error posting appointment: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('Appointment posted successfully:', data);
+    return data;
+  } catch (error) {
+    console.error('Error posting appointment:', error);
+    throw error;
+  }
 }
