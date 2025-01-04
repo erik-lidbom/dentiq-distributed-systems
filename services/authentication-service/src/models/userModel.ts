ff9cfbde289bc8cbbc2a9328f945ac7b9152f042
eriklidb <eriklidb@student.chalmers.se>
2025-01-05 00:48:34 +0100

    #7 - Add additional fields for creating an account

--- services/authentication-service/src/models/userModel.ts
+++ services/authentication-service/src/models/userModel.ts
@@ -6,6 +6,8 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  personnummer: { type: String, required: true },
+  fullname: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['patient', 'dentist'], required: true },
 });
