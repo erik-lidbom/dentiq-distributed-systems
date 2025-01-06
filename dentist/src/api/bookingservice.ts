565757c8c9329708d1ea781f008cf5436410f88d
Andreas <haatajaa@net.chalmers.se>
2025-01-06 22:55:32 +0100

    #38 Add deleteAppointment for deleting appointments.

--- dentist/src/api/bookingservice.ts
+++ dentist/src/api/bookingservice.ts
@@ -1,7 +1,8 @@
 const QUERY_URL = 'http://localhost:3000/api/booking/query';
 const POST_URL = 'http://localhost:3000/api/booking/create';
+const DELETE_URL = 'http://localhost:3000/api/booking/delete';
 
-export async function fetchAppointments(dentistId: string, date: string): Promise<any> {
+export async function fetchAppointments(dentistId: string, date?: string): Promise<any> {
   const response = await fetch(QUERY_URL, {
     method: 'POST',
     headers: {
@@ -9,7 +10,7 @@ export async function fetchAppointments(dentistId: string, date: string): Promis
     },
     body: JSON.stringify({ dentistId, date })
   });
-
+  console.log("RESPONSE IS: ", response)
   if (!response.ok) {
     throw new Error(`Failed to fetch appointments: ${response.statusText}`);
   }
@@ -31,4 +32,26 @@ export async function postAppointments(body: any): Promise<any> {
       }
     
       return response.json()
+}
+
+export async function deleteAppointment(appointmentId: string): Promise<any> {
+    try {
+      const response = await fetch(DELETE_URL, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ appointmentId })
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error deleting booked slot: ${response.statusText}`);
+      }
+      const data = await response.json();
+      console.log('Booking cancelled successfully:', data);
+      return data;
+    } catch (error) {
+      console.error('Error cancelling booking:', error);
+      throw error;
+    }
 }
