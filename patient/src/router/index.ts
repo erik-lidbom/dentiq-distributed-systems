f83ab41d60875d3b2e34f61d314a6658d7e80aa1
nabil-alsayed <nabilsa@chalmers.se>
2025-01-08 15:14:37 +0100

    #7 Improve Path Handling to Allow for Path with No userId and the Opposite

--- patient/src/router/index.ts
+++ patient/src/router/index.ts
@@ -6,12 +6,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:userId',
+      path: '/:userId?',
       name: 'home',
       component: Home,
     },
     {
-      path: '/bookings',
+      path: '/bookings/:userId?',
       name: 'bookings',
       component: Bookings,
     },
