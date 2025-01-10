08751ce2cb92a67141c3d05250d9cf9444b6f94d
Lilly <lilly@chalmers.se>
2025-01-10 01:11:17 +0100

    #50 Fix name issue in vitest setup

--- client/vitest.setup.ts
+++ client/vitest.setup.ts
@@ -4,7 +4,7 @@ import { config } from '@vue/test-utils';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createRouter, createWebHistory } from 'vue-router';
 import SignupView from '@/views/SignupView.vue';
-import LoginView from '@/views/LogInView.vue';
+import LoginView from '@/views/LoginView.vue';
 
 // Set up Vue Router with the routes for client
 const router = createRouter({
